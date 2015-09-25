5630c059e15849aadb12529926d8bca38d8b6a32
Chukwunonso Ibenegbu <jollynony@yahoo.com>
2015-09-25 16:01:46 +0100

    First test passing

--- test_app.py
+++ test_app.py
@@ -6,7 +6,23 @@ import unittest
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class AcceptanceTests(unittest.TestCase):
+    
 
-assert 'Annotation Tags' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_first_three_are_clone_information(self):
+        # We want to see the 3 Clone Information tags appearing first
+        self.browser.get('http://localhost:8000')
+        self.assertIn('Annotation Tags', self.browser.title)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
