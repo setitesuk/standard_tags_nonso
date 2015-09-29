cfdc15aab0235f63e4e2a0012804ac0b4296d97b
Chukwunonso Ibenegbu <jollynony@yahoo.com>
2015-09-29 22:04:35 +0100

    Ready for first submission

--- test_app.py
+++ test_app.py
@@ -10,11 +10,17 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 
 class AcceptanceTests(unittest.TestCase):
-    
+    """
+    This acceptance test is a functional test and as such does not isolate
+    test cases as we would find in a unit test
+    """
 
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
+
+        # Choose wether to use CONTROL or COMMAND for copying and pasting
+        # depending on platform
         self.command_key = Keys.COMMAND if platform.mac_ver()[0] else Keys.CONTROL
 
     def tearDown(self):
@@ -22,7 +28,8 @@ class AcceptanceTests(unittest.TestCase):
 
     def test_first_three_are_clone_information(self):
         """
-         We want to see the 3 Clone Information tags appearing first
+        We want to see the 3 Clone Information tags appearing first
+        and to demonstrate that the user can copy and paste a textarea
         """ 
         # As sanity check confirm that we have title 
         self.browser.get('http://localhost:8000')
@@ -52,25 +59,22 @@ class AcceptanceTests(unittest.TestCase):
         self.assertEqual(end_tag.text, expected_text)
 
         # 
-        end_tag_feature = self.browser.find_element_by_id('feature-list')
-        actions = ActionChains(self.browser)
-        actions.move_to_element(end_tag_feature)
-        actions.click(end_tag_feature)
-        actions.perform()
-        sleep(2)
-        end_tag_feature.send_keys(self.command_key, 'a')
-        end_tag_feature.send_keys(self.command_key, 'c')
+        end_tag_all = self.browser.find_element_by_class_name('md-list-item-text')
+        end_tag_all.send_keys(self.command_key, 'a')
+        sleep(10)
+        end_tag_all.send_keys(self.command_key, 'c')
         paste_target = self.browser.find_element_by_id('paste-content')
         paste_target.clear()
+        sleep(2)
         paste_target.send_keys(self.command_key, 'v')
         value = paste_target.get_attribute('value')
+        end_tag_feature = self.browser.find_element_by_id('feature-list')
         end_tag_feature = "Start/End"
-        print(Keys.CONTROL, "Key Control")
-        print(Keys.COMMAND, "Key Control")
         self.assertIn(end_tag_feature, value)
 
         # Text that is definitely not part of end tag should not appear
         self.assertNotIn('start of', value)
+        sleep(7)
 
         ## Sanity Check
         # Currently, selecting the target element 'end tag text' fails
